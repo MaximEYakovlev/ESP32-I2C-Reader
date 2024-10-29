0fe3eafbabba8c5204bba17d64f7002148a63791
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2024-10-29 16:41:55 +0300

    added: getting the device address

--- main/main.c
+++ main/main.c
@@ -2,16 +2,15 @@
 #include "driver/i2c.h"
 #include "esp_log.h"
 
-#define I2C_MASTER_SCL_IO 2       // SCL
-#define I2C_MASTER_SDA_IO 3       // SDA
-#define I2C_MASTER_NUM I2C_NUM_0  // I2C port
-#define I2C_MASTER_FREQ_HZ 100000 // frequency
-#define DEVICE_ADDR 0x50          // device address
-#define MEMORY_ADDR 0x00          // memory address
+#define I2C_MASTER_SCL_IO 2
+#define I2C_MASTER_SDA_IO 3
+#define I2C_MASTER_NUM I2C_NUM_0
+#define I2C_MASTER_FREQ_HZ 50000
+#define MEMORY_ADDR 0x00
 
 static const char *TAG = "I2C_Reader";
+uint8_t DEVICE_ADDR = 0;
 
-// setting up I2C
 void i2c_master_init()
 {
     i2c_config_t conf;
@@ -25,7 +24,32 @@ void i2c_master_init()
     i2c_driver_install(I2C_MASTER_NUM, conf.mode, 0, 0, 0);
 }
 
-// data reading
+uint8_t i2c_scan_device()
+{
+    for (uint8_t addr = 0x03; addr <= 0x77; addr++)
+    {
+        i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+        i2c_master_start(cmd);
+        i2c_master_write_byte(cmd, (addr << 1) | I2C_MASTER_WRITE, true);
+        i2c_master_stop(cmd);
+
+        esp_err_t ret = i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 1000 / portTICK_PERIOD_MS);
+        i2c_cmd_link_delete(cmd);
+
+        if (ret == ESP_OK)
+        {
+            ESP_LOGI(TAG, "Found device at address: 0x%02X", addr);
+            return addr;
+        }
+        else
+        {
+            ESP_LOGD(TAG, "No response at address 0x%02X", addr);
+        }
+    }
+    ESP_LOGE(TAG, "No I2C device found!");
+    return 0;
+}
+
 esp_err_t i2c_master_read_memory(uint8_t mem_addr, uint8_t *data, size_t len)
 {
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
@@ -46,11 +70,17 @@ void app_main()
     i2c_master_init();
     ESP_LOGI(TAG, "I2C initialized");
 
-    uint8_t data[16]; // data buffer
+    DEVICE_ADDR = i2c_scan_device();
+    if (DEVICE_ADDR == 0)
+    {
+        ESP_LOGE(TAG, "Device not found, exiting.");
+        return;
+    }
+
+    uint8_t data[16];
     if (i2c_master_read_memory(MEMORY_ADDR, data, sizeof(data)) == ESP_OK)
     {
-        // data output to the console
-        printf("Data from device: ");
+        printf("Data from device at address 0x%02X: ", DEVICE_ADDR);
         for (int i = 0; i < sizeof(data); i++)
         {
             printf("0x%02X ", data[i]);
